5c1256ef8fb95727040cd4828ddaa527f62b0b23
iykequame <izaqkull@yahoo.com>
2018-02-18 11:58:26 +0000

    woods: Oreo bring up

---
+++ patches/check-patches.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd ../../../..
+cd system/core
+git apply -v --check ../../device/motorola/woods/patches/0001-system_core.patch
+cd ../..
+cd hardware/interfaces
+git apply -v --check ../../device/motorola/woods/patches/0002-hardware_interfaces.patch
+cd ../..
---
+++ vendorsetup.sh
@@ -0,0 +1,4 @@
+#
+for var in eng user userdebug; do
+  add_lunch_combo lineage_woods-$var
+done
