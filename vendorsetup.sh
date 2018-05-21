c061b1ede5dfbbea10134ff3557c6b85ee50ed7b
Izaq Kull <izaqkull@Strec.GH>
2018-05-21 22:43:56 +0000

    fix

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,21 +6,21 @@ done
 # Patches
 cd frameworks/native
 git reset --hard && git clean -f -d
-patch -p1 < ../../device/motorola/nicklaus/patches/01-frameworks-native.patch
+patch -p1 < ../../device/motorola/woods/patches/01-frameworks-native.patch
 cd ../..
 cd system/core
 git reset --hard && git clean -f -d
-patch -p1 < ../../device/motorola/nicklaus/patches/02-init-HACK-re-allow-services-without-selinux-contexts-defined.patch
-patch -p1 < ../../device/motorola/nicklaus/patches/03-libnetutils-add-MTK-bits-ifc_ccmni_md_cfg.patch
-patch -p1 < ../../device/motorola/nicklaus/patches/04-init-double-the-number-of-available-environment-variables.patch
+patch -p1 < ../../device/motorola/woods/patches/02-init-HACK-re-allow-services-without-selinux-contexts-defined.patch
+patch -p1 < ../../device/motorola/woods/patches/03-libnetutils-add-MTK-bits-ifc_ccmni_md_cfg.patch
+patch -p1 < ../../device/motorola/woods/patches/04-init-double-the-number-of-available-environment-variables.patch
 cd ../..
 cd frameworks/av
 git reset --hard && git clean -f -d
-patch -p1 < ../../device/mediatek/mt6737-common/patches/05-Add-MTKEngineerMode-support-av.patch
+patch -p1 < ../../device/motorola/woods/patches/05-Add-MTKEngineerMode-support-av.patch
 cd ../..
 cd frameworks/base
 git reset --hard && git clean -f -d
-patch -p1 < ../../device/mediatek/mt6737-common/patches/06-Add-MTKEngineerMode-support-base.patch
+patch -p1 < ../../device/motorola/woods/patches/06-Add-MTKEngineerMode-support-base.patch
 cd ../..
 
 echo " PATCH DONE !!!"
