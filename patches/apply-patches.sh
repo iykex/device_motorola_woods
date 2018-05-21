185753019fbf74e80bb6ed918f7fbd76547aaa73
Izaq Kull <izaqkull@Strec.GH>
2018-05-21 22:00:23 +0000

    AOSP patches

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 cd ../../../..
-#cd frameworks/av
-#git apply -v ../../device/motorola/woods/patches/0001-frameworks_av.patch
-#cd ../..
-cd frameworks/base
-git apply -v ../../device/motorola/woods/patches/0002-frameworks_base.patch
-cd ../..
 cd frameworks/native
-git apply -v ../../device/motorola/woods/patches/0003-frameworks_native.patch
-cd ../..
-cd system/netd
-git apply -v ../../device/motorola/woods/patches/0004-system_netd.patch
+git reset --hard && git clean -f -d
+patch -p1 < ../../device/motorola/nicklaus/patches/01-frameworks-native.patch
 cd ../..
 cd system/core
-git apply -v ../../device/motorola/woods/patches/0005-system_core.patch
+git reset --hard && git clean -f -d
+patch -p1 < ../../device/motorola/nicklaus/patches/02-init-HACK-re-allow-services-without-selinux-contexts-defined.patch
+patch -p1 < ../../device/motorola/nicklaus/patches/03-libnetutils-add-MTK-bits-ifc_ccmni_md_cfg.patch
+patch -p1 < ../../device/motorola/nicklaus/patches/04-init-double-the-number-of-available-environment-variables.patch
+cd ../..
+cd frameworks/av
+git reset --hard && git clean -f -d
+patch -p1 < ../../device/mediatek/mt6737-common/patches/05-Add-MTKEngineerMode-support-av.patch
+cd ../..
+cd frameworks/base
+git reset --hard && git clean -f -d
+patch -p1 < ../../device/mediatek/mt6737-common/patches/06-Add-MTKEngineerMode-support-base.patch
 cd ../..
