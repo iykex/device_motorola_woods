0c673573422f5c18d585695aeab641b1f5ffe88c
Izaq Kull <izaqkull@Strec.GH>
2018-05-22 22:35:38 +0000

    woods: stg(1/xx )

--- config.fs
+++ config.fs
@@ -1,10 +1,10 @@
-[bin/hw/android.hardware.audio@2.0-service]
+[vendor/bin/hw/android.hardware.audio@2.0-service]
 mode: 0700
 user: AID_AUDIOSERVER
 group: AID_AUDIOSERVER
 caps: SYS_NICE
 
-[bin/hw/android.hardware.bluetooth@1.0-service]
+[vendor/bin/hw/android.hardware.bluetooth@1.0-service]
 mode: 0700
 user: bluetooth
 group: bluetooth
