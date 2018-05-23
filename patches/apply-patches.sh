b32b8da4f8df569b9eb4d31831b693fa06731594
Izaq Kull <izaqkull@Strec.GH>
2018-05-23 16:56:37 +0000

    woods: rev

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 cd ../../../..
 cd system/core
-cd system/core
 git reset --hard && git clean -f -d
 patch -p1 < ../../device/motorola/woods/patches/00-system_core.patch
 git reset --hard && git clean -f -d
