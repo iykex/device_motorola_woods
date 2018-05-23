df1f4cb2c655b5320685d77ce0bd6796adf418d6
Izaq Kull <izaqkull@Strec.GH>
2018-05-23 17:08:01 +0000

    f^k

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -2,15 +2,15 @@
 cd ../../../..
 cd system/core
 git reset --hard && git clean -f -d
-patch -p1 < ../../device/motorola/woods/patches/00-system_core.patch
+git apply -v ../../device/motorola/woods/patches/00-system_core.patch
 git reset --hard && git clean -f -d
-patch -p1 < ../../device/motorola/woods/patches/01-system_core.patch
+git apply -v ../../device/motorola/woods/patches/01-system_core.patch
 cd ../..
 cd hardware/interfaces
 git reset --hard && git clean -f -d
-patch -p1 < ../../device/motorola/woods/patches/02-hardware_interfaces.patch
+git apply -v ../../device/motorola/woods/patches/02-hardware_interfaces.patch
 cd ../..
 cd system/core
 git reset --hard && git clean -f -d
-patch -p1 < ../../device/motorola/woods/patches/03-system_sepolicy.patch
+git apply -v ../../device/motorola/woods/patches/03-system_sepolicy.patch
 cd ../..
--- patches/check-patches.sh
+++ patches/check-patches.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 cd ../../../..
 cd system/core
-git apply -v --check../../device/motorola/woods/patches/00-system_core.patch
-git apply -v --check../../device/motorola/woods/patches/01-system_core.patch
+git apply -v --check ../../device/motorola/woods/patches/00-system_core.patch
+git apply -v --check ../../device/motorola/woods/patches/01-system_core.patch
 cd ../..
 cd hardware/interfaces
-git apply -v --check../../device/motorola/woods/patches/02-hardware_interfaces.patch
+git apply -v --check ../../device/motorola/woods/patches/02-hardware_interfaces.patch
 cd ../..
 cd system/core
-git apply -v --check../../device/motorola/woods/patches/03-system_sepolicy.patch
+git apply -v --check ../../device/motorola/woods/patches/03-system_sepolicy.patch
 cd ../..
