168bcbaaa20ce692cc8b6058bb6dcac6b64c3540
Izaq Kull <izaqkull@Strec.GH>
2018-05-26 16:52:56 +0000

    woods: don't apply patch automatically :(

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,24 +3,31 @@ for var in eng user userdebug; do
   add_lunch_combo lineage_woods-$var
 done
 
-# Patches
 echo
-echo "Applying Patches Automatically ..."
-cd system/core
-git apply -v ../../device/motorola/woods/patches/0001-system_core.patch
-cd ../..
-cd hardware/interfaces
-git apply -v ../../device/motorola/woods/patches/0002-hardware_interfaces.patch
-cd ../..
+echo "DO AWAY PATCHES..."
+echo
+echo "/// Let's see what happens"
+echo " ;)"
+echo
 
-echo "  :( "
-echo "==__==__=="
-
-echo
-echo "Copying 'SkUserConfig.h' & 'SkUserConfigManual.h' to Core ..."
-cp external/skia/include/config/SkUserConfig.h external/skia/include/core
-echo ""
-cp external/skia/include/config/SkUserConfigManual.h external/skia/include/core
-echo
-echo "...!DONE!"
-echo
+# # Patches
+# echo
+# echo "Applying Patches Automatically ..."
+# cd system/core
+# git apply -v ../../device/motorola/woods/patches/0001-system_core.patch
+# cd ../..
+# cd hardware/interfaces
+# git apply -v ../../device/motorola/woods/patches/0002-hardware_interfaces.patch
+# cd ../..
+#
+# echo "  :( "
+# echo "==__==__=="
+#
+# echo
+# echo "Copying 'SkUserConfig.h' & 'SkUserConfigManual.h' to Core ..."
+# cp external/skia/include/config/SkUserConfig.h external/skia/include/core
+# echo ""
+# cp external/skia/include/config/SkUserConfigManual.h external/skia/include/core
+# echo
+# echo "...!DONE!"
+# echo
