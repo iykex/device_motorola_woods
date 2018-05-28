8cf7f8d8934a9b281589b8de882b815de18ba1b3
Izaq Kull <izaqkull@Strec.GH>
2018-05-28 01:57:24 +0000

    *2

--- gatekeeper/Android.mk
+++ gatekeeper/Android.mk
@@ -23,8 +23,8 @@ LOCAL_SRC_FILES := $(call all-c-files-under,src) $(call all-cpp-files-under,src)
 
 LOCAL_C_INCLUDES := \
     $(LOCAL_PATH)/gatekeeper/include \
-    external/scrypt/lib/crypto \
-    $(ANDROID_BUILD_TOP)/system/gatekeeper/include/gatekeeper
+    external/scrypt/lib/crypto
+    #$(ANDROID_BUILD_TOP)/system/gatekeeper/include/gatekeeper
 
 LOCAL_SHARED_LIBRARIES += \
     libgatekeeper \
